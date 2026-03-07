9c8bdd549506bc8cfd2212ca6e6cb5782f1c236c
YashPatel0 <yashpatel22150@gmail.com>
2026-03-07 10:11:13 +0530

    update

--- INFRA/RDS/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "allocated_storage" {}
-
-variable "max_allocated_storage" {}
-
-variable "instance_class" {}
-
-variable "username" {}
-
-variable "password" {}
--- INFRA/S3/output.tf
+++
@@ -1,5 +0,0 @@
-# Output the bucket's website endpoint
-output "website_endpoint" {
-  value       = aws_s3_bucket.pranav.website_endpoint
-  description = "The URL to access the static website"
-}
